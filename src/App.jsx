2752f1ada54e2c24b2509060f5d11ce049e398e2
Tyler Benning <tbenning@github.com>
2024-05-15 01:27:34 +0000

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import './App.css';
+import React from 'react';
+import { Stack, Banner } from '@primer/react/drafts'
+import { ShieldLockIcon } from '@primer/octicons-react';
+import styles from './App.module.css';
 
 function App() {
+  // --- Popover support in React WIP :/ ---
+  // React is still implementing popover support, PR is open and this branch references a build of it
+  // https://github.com/facebook/react/issues/27479
+  // https://github.com/facebook/react/pull/27981
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    
+    <div className="p-5">
+    <Stack gap="normal">
+      <Banner title="Managed settings" hideTitle variant="info" description={<>
+        Some settings on this page can’t be changed because they’re managed by administrators.
+        </>} secondaryAction={<Banner.SecondaryAction>Learn more</Banner.SecondaryAction>}/>
+      <div>
+      <div className={styles.card}>
+        <Stack gap="condensed" direction="horizontal" justify="space-between" align="center">
+          <div className={styles.textContainer}>
+            <h3>Stuff</h3>
+            <p>Stuff</p>
+          </div>
+          <div>
+          <button popovertargetaction="toggle" popovertarget="my-popover" className={styles.readOnlyValue}  >
+            <ShieldLockIcon fill="var(--fgColor-muted)" />
+            <span>Enabled</span>
+          </button>
+          <div popover="auto" id="my-popover" className={styles.popover}>
+            <div className={styles.popoverContent}>
+              <span>This is managed by GitHub</span>
+            </div>
+            </div>
+          </div>
+        </Stack>
+      </div>
+      </div>
+    </Stack>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@primer/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <body data-color-mode="light" data-light-theme="light" data-dark-theme="dark">
+        <App />
+      </body>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
